5d55b14835d07f4ea94f913689f16d77f78c326d
Chikara <matsudachikara@yahoo.co.jp>
2017-02-19 12:59:43 +0000

    Display a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resources :users, only: [:show]
+  
   root  'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
